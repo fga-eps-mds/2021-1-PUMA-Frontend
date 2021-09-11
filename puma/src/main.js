4b8a3897b1339425f36a8412cefc21f3ca255fec
Nicolas Georgeos Mantzos <georgeos.nicolas83@gmail.com>
2021-09-11 15:39:20 -0300

    #12 implementação dos inputs do formulário

--- puma/src/main.js
+++ puma/src/main.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.min.css';
+import {BootstrapVue, IconsPlugin} from "bootstrap-vue"
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.min.css'
 
 new Vue({
   router,
---
+++ puma/src/views/proposta/Cadastro.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid h-100 text-dark">
+    <div class="row justify-content-center align-items-center">
+      <h3 class="mb-4">Cadastrar Proposta</h3>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col col-lg-4">
+        <form>
+          <div class="form-group">
+            <label class="justify-content-lg-start" for="titulo" type="text">Nome</label>
+            <input type="text" class="form-control" id="titulo" placeholder="Nome"/>
+            <div class="valid-feedback">
+              Nome invalido!
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="justify-content-lg-start" for="previsao">Previsão</label>
+            <input type="text" class="form-control" id="previsao" placeholder="Previsao"/>
+          </div>
+          <div class="form-group">
+            <label class="justify-content-lg-start">Detalhamento</label>
+            <textarea class="form-control" id="detalhe" rows="5"></textarea>
+          </div>
+          <div class="form-group">
+            <button class="col-3 btn btn-primary">Cadastrar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Cadastrar Proposta'
+}
+</script>
