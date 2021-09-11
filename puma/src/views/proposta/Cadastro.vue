53225e90a51aadf2cd7a684fa6d84cfbe1eb5883
Nicolas Georgeos Mantzos <georgeos.nicolas83@gmail.com>
2021-09-11 19:50:43 -0300

    #12 implementação dos inputs do formulário

--- puma/src/views/proposta/Cadastro.vue
+++ puma/src/views/proposta/Cadastro.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="container-fluid h-100 text-dark">
+  <div class="container-fluid h-100 text-dark cadastro">
     <div class="row justify-content-center align-items-center">
       <h3 class="mb-4">Cadastrar Proposta</h3>
     </div>
     <div class="row justify-content-center">
       <div class="col col-lg-4">
-        <form>
+        <form @submit.prevent>
           <div class="form-group">
             <label class="justify-content-lg-start" for="titulo" type="text">Nome</label>
-            <input type="text" class="form-control" id="titulo" placeholder="Nome"/>
+            <input type="text" class="form-control" id="titulo" v-model="nome" placeholder="Nome"/>
             <div class="valid-feedback">
               Nome invalido!
             </div>
           </div>
           <div class="form-group">
             <label class="justify-content-lg-start" for="previsao">Previsão</label>
-            <input type="text" class="form-control" id="previsao" placeholder="Previsao"/>
+            <input type="text" class="form-control" id="previsao" v-model="previsao" placeholder="Previsao"/>
           </div>
           <div class="form-group">
             <label class="justify-content-lg-start">Detalhamento</label>
-            <textarea class="form-control" id="detalhe" rows="5"></textarea>
+            <textarea class="form-control" id="detalhe" v-model="detalhamento" rows="5"></textarea>
           </div>
           <div class="form-group">
-            <button class="col-3 btn btn-primary">Cadastrar</button>
+            <button class="col-3 btn btn-primary" @click="submitForm()">Cadastrar</button>
           </div>
         </form>
       </div>
@@ -33,6 +33,18 @@
 <script>
 
 export default {
-  name: 'Cadastrar Proposta'
+  data() {
+    return {
+      nome: '',
+      previsao: '',
+      detalhamento: '',
+    }
+  },
+  methods : {
+    submitForm() {
+      console.log({nome: this.nome, detalhamento: this.detalhamento, previsao: this.previsao});
+    },
+  },
+  name: 'CadastroProposta'
 }
 </script>
