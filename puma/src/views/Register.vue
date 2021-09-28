6232b89b8e7cf5e581f2f97ff7a480448a05b234
Luis Taira <luishptaira@gmail.com>
2021-09-28 14:36:18 -0300

    [#10] Aligining with prototype

--- puma/src/views/Register.vue
+++ puma/src/views/Register.vue
@@ -1,120 +1,186 @@
 <template>
-    <div>
-        <h1> {{greetings}}{{username}} !</h1>
-        <form id="register-form">
-            <div class="input-container">
-                <label for="tipo">Escolha o tipo de usuario:</label>
-                <select name="tipo" id="tipo">
-                    <option value="">Selecione o tipo de usuário</option>
-                    <option value="Aluno">Aluno</option>
-                    <option value="Professor">Professor</option>
-                </select>
-            </div>
-            <div class="input-container">
-                <label>Nome do usuário</label>
-                <input type="text" id="username" v-model="username" placeholder="Digite o seu nome de usuário">
-            </div>
-            <div class="input-container">
-                <label>Sobrenome</label>
-                <input type="text" id="sobrenome" v-model="sobrenome" placeholder="Digite o seu sobrenome">
-            </div>
-            <div class="input-container">
-                <label>E-mail</label>
-                <input type="text" id="email" v-model="email" placeholder="Digite o seu email">
-            </div>
-            <div class="input-container">
-                <label>Matricula</label>
-                <input type="number" id="matricula" v-model="matricula" placeholder="Digite sua matrícula">
-            </div>
-            <div class="input-container">
-                <label>Senha</label>
-                <input type="password" id="senha" v-model="senha" placeholder="Digite sua senha">
-            </div>
-            <div class="input-container">
-                <input type="submit" class="submit-btn" value="Realizar Cadastro" @click.prevent="create" form="register-form">
-            </div>
-        </form>
+  <body>
+    <div class="container">
+      <div class="column">
+        <div class="card">
+          <div class="title-box">CADASTRO DE USUÁRIO</div>
+          <div>
+            <form id="register-form">
+              <input
+                type="text"
+                id="name"
+                placeholder="Nome Completo"
+              />
+              <input
+                type="text"
+                id="email"
+                v-model="email"
+                placeholder="E-mail"
+              />
+              <input
+                type="password"
+                id="senha"
+                v-model="senha"
+                placeholder="Senha"
+              />
+              <input
+                type="password"
+                id="repeat-senha"
+                v-model="senha"
+                placeholder="Repita sua senha"
+              />
+              <select name="tipo" id="tipo" required>
+                <option value="" selected disabled class="disabled">
+                  Que tipo de usuário você é?
+                </option>
+                <option value="Aluno">Aluno</option>
+                <option value="Professor">Professor</option>
+              </select>
+              <input
+                type="number"
+                id="matricula"
+                v-model="matricula"
+                placeholder="Digite sua matrícula"
+              />
+              <input
+                type="submit"
+                class="submit-btn"
+                value="Cadastrar"
+                @click.prevent="create"
+                form="register-form"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
+  </body>
 </template>
 
 <script>
-    import axios from '@/main.js'
+import axios from "@/main.js";
 
-    export default {
-        name : 'Register',
-        data () {
-            return{
-                greetings:'Seja bem vindo ao PUMA ',
-                username:'usuario',
-                sobrenome:'',
-                email: '',
-                matricula: '',
-                senha: ''
-            }
-        },
-        methods:{
-            create(){
-                let newRegister = {
-                    nome: this.username,
-                    sobrenome: this.sobrenome,
-                    email: this.email,
-                    matricula: this.matricula,
-                    senha: this.senha
-                }
-                
-                axios.post("http://localhost:3004/aluno",newRegister)
-                .then((response) =>{
-                    this.$router.push('/');
-                    alert(response);
-                }).catch((e) =>{
-                    alert(e);
-                });
-            }
-        },    
-    
-    }
+export default {
+  name: "Register",
+  data() {
+    return {
+      greetings: "Seja bem vindo ao PUMA ",
+      username: "usuario",
+      sobrenome: "",
+      email: "",
+      matricula: "",
+      senha: "",
+    };
+  },
+  methods: {
+    create() {
+      let newRegister = {
+        nome: this.username,
+        sobrenome: this.sobrenome,
+        email: this.email,
+        matricula: this.matricula,
+        senha: this.senha,
+      };
+
+      axios
+        .post("http://localhost:3004/aluno", newRegister)
+        .then(() => {
+          this.$router.push("/");
+          alert('Cadastro feito com sucesso.');
+        })
+        .catch(() => {
+          alert('Erro no cadastro');
+        });
+    },
+  },
+};
 </script>
 
 <style>
-    #register-form{
-        max-width: 400px;
-        margin: 0 auto;
-    }
-    
-    .input-container{
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 20px;
-    }
-    
-    label{
-        font-weight: bold;
-        margin-bottom: 15px;
-        color: #222;
-        padding: 5px 10px;
-        border-left:4px solid #0060df;
-    }
+body {
+  background-color: #15355e;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  background-color: #ffffff;
+  border-radius: 20px;
+  margin: 20px;
+  padding: 1px;
+  width: 725px;
+  /* transition: all 0.3s ease-out; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+.title-box {
+  font-size: 2.5em;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: #f7db36;
+  padding: 20px;
+}
 
-    select{
-        padding: 5px 10px;
-        width: 300px;
-    }
-    
-    .submit-btn{
-        background-color: #0060df;
-        color: #222; 
-        font-weight: bold;
-        border: 2px solid #222;
-        padding: 10px;
-        font-size: 16px;
-        margin: 0 auto;
-        cursor: pointer;
-        transition: .5s
-    }
+#register-form {
+  display: flex;
+  flex-direction: column;
+}
 
-    .submit-btn:hover{
-        background-color: transparent;
-        color: #f5871f;
-    }
+input,
+select {
+  border-radius: 7px;
+  font-size: 1.1em;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+  margin-left: 70px;
+  margin-right: 70px;
+  padding: 15px;
+}
 
+select:required:invalid {
+  color: gray;
+}
+option[value=""][disabled] {
+  display: none;
+}
+option {
+  color: black;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 15px;
+  color: #222;
+  padding: 5px 10px;
+  border-left: 4px solid #0060df;
+}
+
+.submit-btn {
+  background-color: #f7db36;
+  color: #222;
+  font-weight: bold;
+  border: 2px solid #f7db36;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.submit-btn:hover {
+  background-color: transparent;
+  color: #f5871f;
+}
 </style>
