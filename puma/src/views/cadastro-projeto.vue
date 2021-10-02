5596e0b19fa468e24cbdd44ce948c437d1b81527
Nicolas Mantzos <georgeos.nicolas83@gmail.com>
2021-10-02 14:39:11 -0300

    [12] adding new database scripts

--- puma/src/views/cadastro-projeto.vue
+++ puma/src/views/cadastro-projeto.vue
@@ -8,12 +8,22 @@
       <div class="form-group">
         <textarea v-model.trim="descricao" class="form-control" id="descricao" placeholder="Descreva sua proposta" rows="3"></textarea>
       </div>
-      <div class="form-group mb-3">
-        <input type="file" id="file" ref="myFiles" class="custom-file-input mb-2" @change="previewFiles" multiple>
+<!--      <div class="form-group mb-3">-->
+<!--        <input type="file" id="file" ref="myFiles" class="custom-file-input mb-2" @change="previewFiles" multiple>-->
+<!--      </div>-->
+
+      <div align="center">
+        <form>
+          <input type="file" multiple>
+        </form>
       </div>
+
       <div class="form-group" v-if="operacao !== 'visualizar'">
         <button type="button" class="btn btn-primary" @click="submitForm">Submeter</button>
       </div>
+<!--      <div class="form-group" v-if="operacao !== 'visualizar'">-->
+<!--        <button type="button" class="btn btn-primary" @click="submitFile">Submeter Arquivo</button>-->
+<!--      </div>-->
 <!--      <div class="form-group">-->
 <!--        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()">-->
 <!--        <button @click="cadastrarArquivo()">Submeter anexo</button>-->
@@ -34,7 +44,6 @@ export default {
       descricao: '',
       titulo: '',
       operacao: 'cadastrar',
-      files: [],
       file: ''
     }
   },
@@ -60,7 +69,7 @@ export default {
           disciplina_aloc: 1
         }
         console.log(projectObject);
-        axios.post("http://localhost:3000/projeto/cadastro",projectObject);
+        axios.post('http://localhost:3000/projeto/cadastro',projectObject);
       }
     },
     cadastrarArquivo() {
@@ -70,8 +79,20 @@ export default {
       // console.log(this.arquivo);
     },
     previewFiles() {
-      this.files = this.$refs.myFiles.files
-      console.log(this.files);
+      this.file = this.$refs.myFiles.files[0]
+      console.log(this.file);
+    },
+    submitFile() {
+      const formData = new FormData();
+      formData.append('file', this.file);
+      try {
+        axios.post('http://localhost:3000/projeto/cadastro-arquivo', formData).then((response) => {
+          console.log(response);
+        });
+      } catch(err) {
+        console.log(err);
+      }
+      console.log(formData);
     },
     validateFormData() {
         return true;
@@ -81,5 +102,54 @@ export default {
 </script>
 
 <style>
+form{
+
+  /*margin-top: -100px;*/
+  /*margin-left: -250px;*/
+  margin-bottom: 25px;
+  width: 500px;
+  height: 200px;
+  border: 4px dashed blue;
+}
+form p{
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  line-height: 170px;
+  color: red;
+  font-family: Arial;
+}
+form input{
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  outline: none;
+  opacity: 0;
+}
+form button{
+  margin: 0;
+  color: #fff;
+  background: #16a085;
+  border: none;
+  width: 508px;
+  height: 35px;
+  /*margin-top: -20px;*/
+  /*margin-left: -4px;*/
+  border-radius: 4px;
+  border-bottom: 4px solid #117A60;
+  transition: all .2s ease;
+  outline: none;
+}
+form button:hover{
+  background: #149174;
+  color: #0C5645;
+}
+form button:active{
+  border:0;
+}
+
+
+
 
 </style>
