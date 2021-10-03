17382c01ad440dc2a966a5ad5a0bb255b5a29971
Luis Taira <luishptaira@gmail.com>
2021-10-03 01:21:47 -0300

    [#11] Adding comment tip for what to do next

--- puma/src/services/userService.js
+++ puma/src/services/userService.js
@@ -1,4 +1,21 @@
 import axios from "@/main.js";
+// import * as Cookie from 'js-cookie';
+// Cookie.set('USER_SESSION', res.user_session, {expires: 7, path: '/'});
+// public get userValue(): User {
+//   return this.userSubject.value;
+// }
+
+// public get userSession(): string {
+// return Cookie.get('USER_SESSION');
+// }
+
+// public get isLoggedIn(): boolean {
+// return Cookie.get('USER_SESSION') ? (Cookie.get('USER_SESSION') != '' ? true : false) : false;
+// }
+
+// public emailIsValid (email) {
+//   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+// }
 
 export default class UserService {
   registerUser(name, email, password, type, matricula) {
