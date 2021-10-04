846257ab4cf1ea028f0c61658bdc932ec2e92c1e
Luis Taira <luishptaira@gmail.com>
2021-10-04 19:01:12 -0300

    [#11] Adding cookie saving

--- puma/src/services/userService.js
+++ puma/src/services/userService.js
@@ -1,26 +1,25 @@
 import axios from "@/main.js";
-// import * as Cookie from 'js-cookie';
-// Cookie.set('USER_SESSION', res.user_session, {expires: 7, path: '/'});
+import * as Cookie from 'js-cookie';
 // public get userValue(): User {
-//   return this.userSubject.value;
-// }
-
-// public get userSession(): string {
-// return Cookie.get('USER_SESSION');
-// }
-
-// public get isLoggedIn(): boolean {
-// return Cookie.get('USER_SESSION') ? (Cookie.get('USER_SESSION') != '' ? true : false) : false;
-// }
-
-// public emailIsValid (email) {
-//   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-// }
-
-export default class UserService {
-  registerUser(newUser) {
-    return new Promise((resolve, reject) => {
-
+  //   return this.userSubject.value;
+  // }
+  
+  // public get userSession(): string {
+    // return Cookie.get('USER_SESSION');
+    // }
+    
+    // public get isLoggedIn(): boolean {
+      // return Cookie.get('USER_SESSION') ? (Cookie.get('USER_SESSION') != '' ? true : false) : false;
+      // }
+      
+      // public emailIsValid (email) {
+        //   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+        // }
+        
+        export default class UserService {
+          registerUser(newUser) {
+            return new Promise((resolve, reject) => {
+              
       axios.post(`${global.URL_GATEWAY}/register`, newUser).then((response) => {
         resolve("/register resolve: " + response);
       }).catch((response) => {
@@ -28,12 +27,17 @@ export default class UserService {
       });
     });
   }
-
+  
   logUserIn(newUser) {
     return new Promise((resolve, reject) => {
-
+      
       axios.post(`${global.URL_GATEWAY}/login`, newUser).then((response) => {
-        resolve("/login resolve: " + response);
+        if(response.data.auth) {
+          Cookie.set('PUMA_USER_SESSION', response.data.token, {expires: 7, path: '/'});
+          resolve("/login resolve: " + response);
+        } else {
+          reject("/login reject: " + response);  
+        }
       }).catch((response) => {
         reject("/login reject: " + response);
       });
