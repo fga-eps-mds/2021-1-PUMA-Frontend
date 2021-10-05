f101a5512f9f5970eca8e6f0bfff48e93f3ca578
Eduardo Lima <eduardolima.df@gmail.com>
2021-10-05 00:24:54 -0300

    [#93] Adding lint structure

--- puma/src/router/index.js
+++ puma/src/router/index.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/about',
@@ -16,20 +16,21 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
     path: '/register',
-    name: 'Register' ,
-    component: () => import ('@/views/Register.vue')
+    name: 'Register',
+    // eslint-disable-next-line import/no-unresolved
+    component: () => import('@/views/Register.vue'),
 
-  }
-]
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
