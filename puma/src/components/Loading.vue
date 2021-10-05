21de303c7a62c5d75f54dfa7eb2eecd71beea97e
Eduardo Lima <eduardolima.df@gmail.com>
2021-10-05 02:46:10 -0300

    [#11] Fixing vue files lint

--- puma/src/components/Loading.vue
+++ puma/src/components/Loading.vue
@@ -8,7 +8,7 @@
 
 <script>
 export default {
-  name: "Loading",
+  name: 'Loading',
 };
 </script>
 
--- puma/src/components/Navbar.vue
+++ puma/src/components/Navbar.vue
@@ -1,29 +1,29 @@
 <template>
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/register">Register</router-link>
-    </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/register">Register</router-link>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Navbar'
-    }
+export default {
+  name: 'Navbar',
+};
 </script>
 
 <style scope>
-    #nav {
-        padding: 30px;
-        background-color: #15355e ;
-    }
+#nav {
+  padding: 30px;
+  background-color: #15355e;
+}
 
-    #nav a {
-        font-weight: bold;
-        color: white ;
-    }
+#nav a {
+  font-weight: bold;
+  color: white;
+}
 
-    #nav a.router-link-exact-active {
-        color: #42b983;
-    }
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
