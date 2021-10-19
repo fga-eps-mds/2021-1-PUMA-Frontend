b1a80891f5f0a8d2bcba51c05c3cc58e202ab132
Luis Taira <luishptaira@gmail.com>
2021-10-19 02:47:06 -0300

    [#26] Creating project card
    Co-authored-by: Gabriel Tiveron <gabrielmtiveron@gmail.com>

--- puma/src/views/Evaluate/evaluate.js
+++ puma/src/views/Evaluate/evaluate.js
@@ -1,17 +1,21 @@
 import ProjectService from '../../services/projectService';
+// eslint-disable-next-line import/no-unresolved
+import ProjectCard from '@/components/ProjectCard.vue';
 
 const projectService = new ProjectService();
 
 export default {
-  components: { },
+  components: {
+    ProjectCard,
+  },
   name: 'Evaluate',
   data() {
-    return { };
+    return {};
   },
   created() {
     projectService.getAllocatedProjects(1).then((lel) => {
       console.log(lel);
     });
   },
-  methods: { },
+  methods: {},
 };
