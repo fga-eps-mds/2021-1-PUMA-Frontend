50126c1341fab9b5738397eab199b03364b8e7ac
Luís Henrique Pereira Taira <luishptaira@gmail.com>
2021-10-27 18:27:52 -0300

    Update Home.vue

--- puma/src/views/Home.vue
+++ puma/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Puma logo" src="../assets/puma.png" />
+    <router-link :to="{ path: '/evalueate/1'}" replace>PSP1</router-link>
+    <router-link :to="{ path: '/evalueate/2'}" replace>PSP2</router-link>
   </div>
 </template>
 
