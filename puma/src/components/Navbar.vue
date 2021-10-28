1d1ab28d0f1f7bd4dc4fd6f144c7f4525f849075
Luis Taira <luishptaira@gmail.com>
2021-10-28 17:26:14 -0300

    [#106] Fininshing navbar style
    Co-authored-by: Gabriel Tiveron <gabrielmtiveron@gmail.com>

--- puma/src/components/Navbar.vue
+++ puma/src/components/Navbar.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/register">Register</router-link> |
-    <router-link to="/evaluate">Evaluate</router-link>
+  <div id='nav'>
+    <img class='nav-item' id='logo' src='../assets/navbarLogo.svg' />
+    <div id='dropdown'>
+      <img class='nav-item' id='menu-button' src='../assets/navbarMenu.svg' />
+      <div class="dropdown-content">
+        <a href="#">Meu perfil</a>
+        <a href="#">Configurações</a>
+        <a href="#">Sair</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,14 +19,69 @@ export default {
 </script>
 
 <style scope>
-#nav {
-  padding: 30px;
-  background-color: #15355e;
+div {
+  position: relative;
 }
 
-#nav a {
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #15355e;
+  width: 300px;
+  transform: translate(-180px);
+  z-index: 1;
+  font-size: 20px;
+  font-family: rubik, sans-serif;
+  font-weight: 300;
+}
+
+.dropdown-content a {
+  height: 35px;
+  color: white;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+#dropdown {
+  margin-top: 5px;
+}
+
+.dropdown-content a:hover {
+  background-color: #265FAB;
+}
+
+#dropdown:hover .dropdown-content {
+  display: block;
+}
+
+#nav {
+  padding: 7 px;
+  background-color: #15355e;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
   font-weight: bold;
   color: white;
+  align-items: center;
+}
+
+#logo {
+  height: 18px;
+}
+
+#menu {
+  position: absolute;
+}
+
+#menu-button {
+  height: 55px;
+}
+
+.nav-item {
+  margin-left: 45px;
+  margin-right: 20px;
 }
 
 #nav a.router-link-exact-active {
