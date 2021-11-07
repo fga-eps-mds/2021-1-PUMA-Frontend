e3534e1f9ce67497aa0002a0abdd672bad9cf76c
jpmartins201 <jpmartins201@live.com>
2021-11-07 20:38:33 -0300

    [#86] Refactoring subject listing

--- puma/src/views/Subject/MySubjects/mySubjects.js
+++ puma/src/views/Subject/MySubjects/mySubjects.js
@@ -1,34 +1,32 @@
-import ProjectService from '../../../services/projectService';
+import SubjectService from '../../../services/subjectService';
 // eslint-disable-next-line import/no-unresolved
 import ProjectCard from '@/components/ProjectCard.vue';
 
-const projectService = new ProjectService();
+const subjectService = new SubjectService();
 
 export default {
   components: {
     ProjectCard,
   },
-  name: 'Approval',
+  name: 'My Subjects',
   data() {
     return {
-      projs: [],
+      subjects: [],
     };
   },
-  methods: {
-    goToNewProposal() {
-      this.$router.push({ name: 'Cadastro de Projeto' });
-    },
-    doNothing() {
-      // pass
-    },
-  },
   created() {
-    projectService.getMyProposals().then((proposals) => {
-      proposals.data.forEach((element) => {
-        this.projs.push({ ...element, is_last: false, submit: this.doNothing });
-      });
-      // this.projs = proposals.data;
-      this.projs.push({ is_last: true, submit: this.goToNewProposal });
+    subjectService.getSubjects().then((res) => {
+      console.log(res.data);
     });
+  },
+  methods: {
+    subjectDetails(subject) {
+      this.$router.push({
+        name: 'Disciplina',
+        params: {
+          subjectId: subject.id,
+        },
+      });
+    },
   },
 };
